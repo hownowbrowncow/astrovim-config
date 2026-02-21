64a38ff45f8df36b26637433c0146c48c2ff5bd3
nick <me@nickc.io>
2026-02-21 09:21:46 -0500

    feat: Add neotree config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+-- Customize Neo-tree file explorer
+
+---@type LazySpec
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function() require("neo-tree.command").execute { action = "close" } end,
+      },
+    },
+  },
+}
